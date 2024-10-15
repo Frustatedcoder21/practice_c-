c7527814026e5d01a827db04d4ded5b05b7b33a5
Frustatedcoder21 <mayankp2k1@gmail.com>
2024-10-15 19:22:52 +0530

    new stack code pushed

--- stack/stack_implementation_using_linkedlistcpp.cpp
+++ stack/stack_implementation_using_linkedlistcpp.cpp
@@ -1,62 +1,73 @@
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-class node{
+
+class Node{
     public:
     int data;
-    node * next;
-    node(int value){
-        data=value;
-        next=NULL;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = NULL;
     }
 };
-class stack{
-    public:
-    node* top;
+
+class Stack{
+    Node* top;
     int size;
-    stack(){
-        top=NULL;
-        size=0;
+    
+    public:
+    Stack(){
+        top = NULL;
+        size = 0;
     }
-    void push(int value){
-        node * temp=new node(value);
-        temp->next=top;
-        top=temp;
-        size++;
+
+    void push(int data){
+        Node* temp = new Node(data);
+        if(temp == NULL){
+            cout << "Stack Overflow\n";
+            return;
+        }
+        temp->next = top;
+        top = temp;
+        size++;  // Increment the size here
+        cout << top->data << " pushed into the stack\n";
     }
+
     void pop(){
-        if(top==NULL){
-            cout<<"stack underflow\n";
-        }else{
-            node * temp=top;
-            top=top->next;
-            cout<<temp->data<<" is poped\n";
-            delete temp;
-            size--;
+        if(top == NULL){
+            cout << "Stack Underflow\n";
+            return;
+        }
+        Node* temp = top;
+        cout << top->data << " popped out of the stack\n";
+        top = top->next;
+        delete temp;
+        size--;  // Decrement the size when popping
+    }
+
+    int peek(){
+        if(top == NULL){
+            return -1;  // Return -1 when the stack is empty
+        } else {
+            return top->data;
         }
     }
-    void peek(){
-    if(top==NULL){
-            cout<<"stack is empty\n";
-        }else{
-            cout<<top->data<<" is top elememt\n";
-        }
-        
+
+    bool isEmpty(){
+        return top == NULL;
     }
-    
-    
+
+    int getSize(){
+        return size;  // Return the correct size of the stack
+    }
 };
 
-int main()
-{ stack s1;
- s1.push(1);
- s1.push(2);
- s1.push(3);
- s1.peek();
- //s1.Issize();
- s1.pop();
- s1.peek();
- // s1.Issize();
-
- 
+int main(){
+    Stack s;
+    s.push(5);
+    s.push(10);
+    cout << "Top element is: " << s.peek() << endl;
+    s.pop();
+    cout << "Stack size is: " << s.getSize() << endl;
+    return 0;
 }
